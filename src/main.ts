595dc92c10439d9517a19b04548d9f6a51c3857b
richegg <zxp86021@gmail.com>
2020-09-14 10:51:38 +0800

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('WeMo Pre-Exam')
+    .setDescription('The Pre-Exam of WeMo')
+    .setVersion('1.0')
+    .addTag('WeMo')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
