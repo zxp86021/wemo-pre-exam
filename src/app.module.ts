92faea89dc585d562189e5885dbdd5f800e97173
richegg <zxp86021@gmail.com>
2020-09-21 16:17:47 +0800

    clean code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BikesController } from './bikes/bikes.controller';
-import { BikesService } from './bikes/bikes.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { BikeModule } from './modules/bike.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), BikeModule],
-  controllers: [AppController, BikesController],
-  providers: [AppService, BikesService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {
   constructor(private connection: Connection) {}
